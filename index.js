671728b354f5e22d3b78ac23578888313f2b74f5
Pavan Sadarangani <psadarangani@salesforce.com>
2018-07-12 16:07:24 +0100

    styling

--- index.js
+++ index.js
@@ -168,7 +168,26 @@ app.post('/personalisedFeed', (req, res) => {
 			console.log('returned some data');
 			let content = '';
 			for (let i in trips.products) {
-				content+= `<div><h2 style="color:#808080;font-family:arial,helvetica,sans-serif;font-size:22px;font-style:normal;font-weight:bold;line-height:1;">${trips.products[i].hotel.location.country.name} nur&nbsp;&nbsp;<b><span style="color:#800000;">${trips.products[i].price.amountTotal} &euro; </span></b></h2><h3 style="color:#808080;font-family:arial,helvetica,sans-serif;font-size:20px;font-style:normal;font-weight:bold;line-height:1;">Ihr Hotel in ${trips.products[i].hotel.location.city.name}&nbsp;ist&nbsp;${trips.products[i].hotel.name} der Kategorie:&nbsp;${trips.products[i].hotel.category}&nbsp;</h3><b><span style="font-size:16px;">Reisedaten für &nbsp;${trips.products[i].travelPeriod.duration} Tage:</span></b><div><span style="font-size:13px;"><b>Abflug:</b></span></div><div><span style="font-size:13px;">${trips.products[i].flight.inbound.arrivalAirport.name} to ${trips.products[i].flight.inbound.departureAirport.name} on ${moment(trips.products[i].flight.inbound.departureDateTime).format('DD-MM-YY')} at ${moment(trips.products[i].flight.inbound.departureDateTime).format('HH:mm')}</span></div><div><span style="font-size:13px;"><b>R&uuml;ckflug:</b></span></div><div><span style="font-size:13px;">${trips.products[i].flight.outbound.arrivalAirport.name} to ${trips.products[i].flight.outbound.departureAirport.name} on ${moment(trips.products[i].flight.outbound.departureDateTime).format('DD-MM-YY')} at ${moment(trips.products[i].flight.outbound.departureDateTime).format('HH:mm')}</span></div></div>`;
+				content+= `<table align="left" border="0" cellpadding="8" cellspacing="0" style="width:100%;">
+								<tr>
+									<td style="background-color: rgb(203, 7, 102);"><span style="font-size:16px;"><span style="font-family:arial,helvetica,sans-serif;"><span style="color:#FFFFFF;"><b>${trips.products[i].hotel.location.country.name} nur ${trips.products[i].price.amountTotal} &euro;</b></span></span></span>
+								</td></tr><tr>
+									<td style="background-color: rgb(246, 140, 18);"><span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Ihr Hotel in ${trips.products[i].hotel.location.city.name} ist ${trips.products[i].hotel.name} der Kategorie ${trips.products[i].hotel.category}&nbsp;</span></span>
+								</td></tr><tr>
+									<td style="background-color: rgb(221, 236, 242); height: 15px;"><b style="font-family: arial, helvetica, sans-serif; font-size: 12px;">Reisedaten f&uuml;r ${trips.products[i].travelPeriod.duration} Tage:</b>
+									</td></tr><tr>
+									<td style="background-color: rgb(248, 248, 248);"><span style="font-family: arial, helvetica, sans-serif; font-size: 12px;"><b>Abflug</b>:</span><span style="font-family: arial, helvetica, sans-serif; font-size: 12px;">${trips.products[i].flight.inbound.arrivalAirport.name} nach ${trips.products[i].flight.inbound.departureAirport.name} am ${moment(trips.products[i].flight.inbound.departureDateTime).format('DD-MM-YY')} um ${moment(trips.products[i].flight.inbound.departureDateTime).format('HH:mm')} Uhr.</span><br><br style="font-family: arial, helvetica, sans-serif; font-size: 12px;"><span style="font-family: arial, helvetica, sans-serif; font-size: 12px;"><b>R&uuml;ckflug</b>:&nbsp;</span><span style="font-family: arial, helvetica, sans-serif; font-size: 12px;">${trips.products[i].flight.outbound.arrivalAirport.name} nach ${trips.products[i].flight.outbound.departureAirport.name} am ${moment(trips.products[i].flight.outbound.departureDateTime).format('DD-MM-YY')} um ${moment(trips.products[i].flight.outbound.departureDateTime).format('HH:mm')} Uhr.</span>
+								</td></tr><tr>
+									<td style="background-color: rgb(221, 236, 242);"><b><span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:12px;">Unterkunft:</span></span></b>
+									</td></tr><tr>
+									<td style="background-color: rgb(248, 248, 248);"><span style="font-size:12px;"><span style="font-family:arial,helvetica,sans-serif;">Zimmer:&nbsp;${trips.products[i].hotel.room.name}<br>Verpflegung:&nbsp;</span></span><span style="font-family: arial, helvetica, sans-serif; font-size: 12px;">${trips.products[i].hotel.board.name}</span>
+									</td></tr><tr>
+									<td style="background-color: rgb(73, 153, 185); text-align: right; height: 3px;">
+									</td></tr><tr>
+									<td style="text-align: right; height: 5px;">
+									</td>
+								</tr>
+						</table>`
 			}
 			console.log(content);
 			res.send(content);
